57186d92156919997c791c2643a153a722aa9ede
Krish Shaw <shawkrish418@gmail.com>
2026-02-01 15:04:05 +0530

    ui: tweeked connect wallet button and modeToggle button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import "./home.css";
 function Home() {
   return (
-    <section className="flex items-center justify-between p-35">
+    <section aria-label="hero" className="flex w-screen items-center justify-between md:p-20 md:my-7 lg:p-30 lg:my-10 overflow-y-hidden">
       <div className="relative">
-        <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-500 rounded-full filter blur-xl opacity-70 animate-blob"></div>
-        <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-500 rounded-full filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-        <div className="absolute -bottom-10 left-20 w-72 h-72 bg-pink-500 rounded-full filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+        <div className="absolute top-0 -left-4 w-72 h-72 bg-linear-to-tr from-purple-500 rounded-full filter blur-xl opacity-70 animate-blob"></div>
+        <div className="absolute top-0 -right-4 w-72 h-72 bg-linear-to-tl  from-pink-700 rounded-full filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+        <div className="absolute -bottom-10 left-20 w-72 h-72 bg-linear-to-bl from-cyan-400 rounded-full filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
         <div className="relative z-10 flex flex-col items-center">
           <div className="flex flex-col">
             <p className="text-1">
@@ -20,7 +20,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <div>
+      <div className="hidden lg:block">
         <div className="cube-container">
             <div className="cube">
                 <div className="cube-3d">
--- components/ui/navbar/navbar.tsx
+++ components/ui/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import "./logo.css";
+import "./navbar.css";
 import Image from "next/image";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ModeToggle } from "../modeToggle";
@@ -12,7 +12,7 @@ function Navbar() {
   const isConnected = useWallet().connected;
 
   return (
-    <nav className="flex w-screen justify-between items-center pr-5 top-0 z-50 sticky bg-neutral-800/30 backdrop-blur-lg">
+    <nav className="flex w-screen justify-between pr-5 top-0 z-50 sticky bg-neutral-800/30 backdrop-blur-lg">
       <Link href="/">
       <div className="flex items-center">
         <Image src="/cubo-logo-centered.svg" alt="cubo-logo" width={70} height={70} className="cropped-svg"/>
@@ -23,20 +23,27 @@ function Navbar() {
         <Tabs defaultValue="home">
           <TabsList variant="line">
             <Link href="/">
-            <TabsTrigger value="home">
+            <TabsTrigger value="home" className="tabs">
               Home
             </TabsTrigger>
             </Link>
-            <TabsTrigger value="create">Create NFTs</TabsTrigger>
             {isConnected &&
-            <Link href="/dashboard">
-            <TabsTrigger value="dashboard">
-              Dashboard
+            <Link href="/tokens">
+            <TabsTrigger value="dashboard" className="tabs">
+              Tokens
             </TabsTrigger>
             </Link>}
           </TabsList>
         </Tabs>
-        <WalletMultiButton />
+        <WalletMultiButton style={{
+          fontFamily: 'Orbitron, sans-serif',
+          backgroundImage: 'linear-gradient(135deg, #21e47f 0%, #68c4f6 100%)',
+          WebkitTextFillColor: 'transparent',
+          WebkitBackgroundClip: 'text',
+          border: "1px solid white",
+          borderRadius: '10px',
+          width: '100%'
+        }}/>
         <ModeToggle/>
       </div>
     </nav>
